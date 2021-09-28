e4ffbf4a65a7aee82da2bc24e096445694ac9dc5
juanbep <jbecapillimue@gmail.com>
2021-09-28 10:06:44 -0500

    ultima version estable

--- API-Menu/src/main/java/co/unicauca/menu/access/MenuRepository.java
+++ API-Menu/src/main/java/co/unicauca/menu/access/MenuRepository.java
@@ -11,74 +11,72 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 /**
  * Es una implementación que tiene libertad de hacer una implementación del
- * contrato. Se utiliza postgres, importante: para que funcione la aplicación, 
- * se debe copiar manualmente la librería de maven de netbeans postgresql-42.2.8.jar, 
- * y pegarla en el directorio donde se haya instalado Payara
+ * contrato. Se utiliza postgres, importante: para que funcione la aplicación,
+ * se debe copiar manualmente la librería de maven de netbeans
+ * postgresql-42.2.8.jar, y pegarla en el directorio donde se haya instalado
+ * Payara
+ *
  * @author Beca98
  */
-public class MenuRepository implements IMenuRepository{
-    
+public class MenuRepository implements IMenuRepository {
+
     private Connection conn;
-    
-    public MenuRepository (){
+
+    public MenuRepository() {
         initDatabase();
     }
-    
-    
 
     /**
-     *  Inicializa la tabla Menu e insertar un registro
+     * Inicializa la tabla Menu e insertar un registro
      */
-    private void initDatabase() 
-    {
-         //SQL statement for creating a new table
+    private void initDatabase() {
+        //SQL statement for creating a new table
         String sql = "CREATE TABLE IF NOT EXISTS menu ("
-                        + "IDMENU varchar(60) PRIMARY KEY," 
-                        + "NAMEMENU varchar(60) NOT NULL,"
-                        + "IDREST varchar(60) not null" 
-                        + "); "
-                
-                        + "CREATE TABLE IF NOT EXISTS visualizacion ("
-                        + "IDMENU varchar(60) not null,"
-                        + "DIA varchar(60) not null,"
-                        + "primary key(IDMENU, DIA)"
-                        + "); "
-                        
-                        + "CREATE TABLE IF NOT EXISTS ofrece("
-                        + "IDMENU varchar(60) not null,"
-                        + "ID varchar(60) not null,"
-                        + "primary key(IDMENU, ID)"
-                         + "); ";
-          try {
+                + "IDMENU varchar(60) PRIMARY KEY,"
+                + "NAMEMENU varchar(60) NOT NULL,"
+                + "IDREST varchar(60) not null"
+                + "); "
+                + "CREATE TABLE IF NOT EXISTS visualizacion ("
+                + "IDMENU varchar(60) not null,"
+                + "DIA varchar(60) not null,"
+                + "primary key(IDMENU, DIA)"
+                + "); "
+                + "CREATE TABLE IF NOT EXISTS ofrece("
+                + "IDMENU varchar(60) not null,"
+                + "ID varchar(60) not null,"
+                + "primary key(IDMENU, ID)"
+                + "); ";
+        try {
             this.connect();
             Statement stmt = conn.createStatement();
             stmt.execute(sql);
             // Datos de inicialización
-            if(this.findByMenuId("100")==null){
-            stmt.execute("INSERT INTO menu(IDMENU, NAMEMENU, IDREST) "
-                    + "values('100','Menu Oriental','500');");
+            if (this.findByMenuId("100") == null) {
+                stmt.execute("INSERT INTO menu(IDMENU, NAMEMENU, IDREST) "
+                        + "values('100','Menu Oriental','500');");
             }
             this.disconnect();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, null, ex);
-        }   
+        }
     }
-    
+
     /**
      * Conectar a la bd
      */
-    public void connect(){
+    public void connect() {
         try {
-            
+
             String url = "jdbc:postgresql://localhost:5432/menuBD";
             String usuario = "postgres";
             String contrasenia = "123";
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection(url, usuario, contrasenia);
-           
+
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -101,18 +99,24 @@ public class MenuRepository implements IMenuRepository{
     @Override
     public List<Menu> findAll() {
         List<Menu> menu = new ArrayList<>();
+        String idMenu;
+        //List<String> dias = new ArrayList<>();
         try {
 
             String sql = "SELECT * FROM menu";
+
             this.connect();
 
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+
             while (rs.next()) {
                 Menu newMenu = new Menu();
                 newMenu.setAtrIdMenu(rs.getString("IDMENU"));
+                idMenu = rs.getString("IDMENU");
                 newMenu.setAtrNomMenu(rs.getString("NAMEMENU"));
                 newMenu.setAtrIdRest(rs.getString("IDREST"));
+                obtenerDias(idMenu, newMenu);
                 menu.add(newMenu);
             }
             this.disconnect();
@@ -127,7 +131,7 @@ public class MenuRepository implements IMenuRepository{
         List<Menu> menu = new ArrayList<>();
         try {
 
-            String sql = "SELECT * FROM menu where idrest='"+IdRest+"'";
+            String sql = "SELECT * FROM menu where idrest='" + IdRest + "'";
             this.connect();
 
             Statement stmt = conn.createStatement();
@@ -145,16 +149,17 @@ public class MenuRepository implements IMenuRepository{
         }
         return menu;
     }
+
     /*
     * Verifica si existe el menu en la base de datos
     * buscando por su primary key idmenu
-    */
+     */
     @Override
     public Menu findByMenuId(String prmIdMenu) {
         Menu menu = null;
         try {
 
-            String sql = "SELECT * FROM menu Where IDMENU='" + prmIdMenu+"'";
+            String sql = "SELECT * FROM menu Where IDMENU='" + prmIdMenu + "'";
             this.connect();
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
@@ -163,7 +168,7 @@ public class MenuRepository implements IMenuRepository{
                 menu.setAtrIdMenu(rs.getString("idmenu"));
                 menu.setAtrNomMenu(rs.getString("namemenu"));
                 menu.setAtrIdRest(rs.getString("idrest"));
-                }
+            }
             this.disconnect();
 
         } catch (SQLException ex) {
@@ -174,7 +179,7 @@ public class MenuRepository implements IMenuRepository{
 
     @Override
     public boolean create(Menu prmNewMenu) {
-        String sql="";
+        String sql = "";
         try {
             this.connect();
 
@@ -201,11 +206,11 @@ public class MenuRepository implements IMenuRepository{
 
             String sql = "UPDATE menu "
                     + "SET  "
-                    + "NAMEMENU ='"+prmNewMenu.getAtrNomMenu()+ "', "
-                    + "IDREST = '"+prmNewMenu.getAtrIdRest()+"' "
-                    + "WHERE IDMENU = '"+prmNewMenu.getAtrIdMenu()+"'";
+                    + "NAMEMENU ='" + prmNewMenu.getAtrNomMenu() + "', "
+                    + "IDREST = '" + prmNewMenu.getAtrIdRest() + "' "
+                    + "WHERE IDMENU = '" + prmNewMenu.getAtrIdMenu() + "'";
             PreparedStatement pstmt = conn.prepareStatement(sql);
-            
+
             pstmt.executeUpdate();
             this.disconnect();
             return true;
@@ -216,21 +221,18 @@ public class MenuRepository implements IMenuRepository{
     }
 
     @Override
-    public boolean delete(String prmIdMenu) 
-    {
-        try 
-        {
+    public boolean delete(String prmIdMenu) {
+        try {
 
             String sql = "DELETE FROM menu "
-                        + "WHERE IDMENU = ?";
+                    + "WHERE IDMENU = ?";
             this.connect();
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, prmIdMenu);
             pstmt.executeUpdate();
             this.disconnect();
             return true;
-        } catch (SQLException ex) 
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, "Error al eliminar el menu", ex);
         }
         return false;
@@ -238,93 +240,113 @@ public class MenuRepository implements IMenuRepository{
 
     @Override
     public boolean createVisualizacion(String prmIdMenu, String prmDia) {
-        String sql="";
+        String sql = "";
         try {
             this.connect();
 
             sql = "INSERT INTO visualizacion ( IDMENU, DIA) "
                     + "VALUES ( '" + prmIdMenu + "','" + prmDia + "')";
-            
+
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.executeUpdate();
             this.disconnect();
             return true;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, "Error en el insert into del menu" + sql, ex);
         }
         return false;
     }
 
     @Override
-    public boolean deleteVisualizacion (String prmIdMenu, String prmDia){
-        String sql="";
+    public boolean deleteVisualizacion(String prmIdMenu, String prmDia) {
+        String sql = "";
         try {
             this.connect();
 
-            sql = "DELETE  FROM visualizacion WHERE IDMENU='"+prmIdMenu+"' and DIA='"+prmDia+"'";
-                    
+            sql = "DELETE  FROM visualizacion WHERE IDMENU='" + prmIdMenu + "' and DIA='" + prmDia + "'";
+
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.executeUpdate();
             this.disconnect();
             return true;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, "Error en el insert into del menu" + sql, ex);
         }
         return false;
     }
-    
+
     @Override
     public boolean createOfrece(String prmIdMenu, String prmIdPlato) {
-         try {
+        try {
             this.connect();
 
-            String sql = "INSERT INTO ofrece (IDMENU,ID) VALUES ('"+prmIdMenu+"','"+prmIdPlato+"')";
-                    
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-             pstmt.executeUpdate();
+            String sql = "INSERT INTO ofrece (IDMENU,ID) VALUES ('" + prmIdMenu + "','" + prmIdPlato + "')";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.executeUpdate();
             this.disconnect();
             return true;
-         } catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, "Error al actualizar", ex);
         }
         return false;
-        
+
     }
 
     @Override
-    public boolean deleteOfrece (String prmIdMenu, String prmIDPlato){
-        String sql="";
+    public boolean deleteOfrece(String prmIdMenu, String prmIDPlato) {
+        String sql = "";
         try {
             this.connect();
 
-            sql = "DELETE  FROM ofrece WHERE IDMENU='"+prmIdMenu+"' and ID='"+prmIDPlato+"'";
-                    
+            sql = "DELETE  FROM ofrece WHERE IDMENU='" + prmIdMenu + "' and ID='" + prmIDPlato + "'";
+
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.executeUpdate();
             this.disconnect();
             return true;
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, "Error en el insert into del menu" + sql, ex);
         }
         return false;
     }
-         
-    
+
+    public void obtenerDias(String prmIdMenu, Menu menu) {
+
+        try {
+
+            String sql = "SELECT * FROM visualizacion WHERE IDMENU = '" + prmIdMenu + "'";
+            this.connect();
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+
+                menu.setAtrDiasVisualizacion(rs.getString("DIA"));
+
+            }
+            this.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(MenuRepository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     @Override
     public List<String> listarDia(String prmIdMenu) {
         List<String> listaDia = null;
         try {
 
-            String sql = "SELECT DIA FROM visualizacion WHERE IDMENU = '" + prmIdMenu + "'";
+            String sql = "SELECT * FROM visualizacion WHERE IDMENU = '" + prmIdMenu + "'";
             this.connect();
 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
+            listaDia = new ArrayList<>();
             while (rs.next()) {
-                listaDia = new ArrayList<>();
-                
+
                 listaDia.add(rs.getString("DIA"));
-                
+
             }
             this.disconnect();
         } catch (SQLException ex) {
@@ -345,9 +367,9 @@ public class MenuRepository implements IMenuRepository{
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 listaPlato = new ArrayList<>();
-                
+
                 listaPlato.add(rs.getString("ID"));
-                
+
             }
             this.disconnect();
         } catch (SQLException ex) {
@@ -355,6 +377,5 @@ public class MenuRepository implements IMenuRepository{
         }
         return listaPlato;
     }
-               
-        
+
 }
